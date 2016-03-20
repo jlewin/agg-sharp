b16f64b00743456b002d4cf0c3d1c6f0a355685a
John Lewin <lewin76@gmail.com>
2016-03-20 13:00:08 -0700

    Configuration driven platform

--- agg/Extensibility/Configuration.cs
+++ agg/Extensibility/Configuration.cs
@@ -31,37 +31,70 @@ namespace MatterHackers.Agg.PlatformAbstract
 		public static ImageIOProvider ImageIO { get; }
 		public static IFileDialogProvider FileDialogs { get; }
 		public static IFrostedSerialPortFactory FrostedSerialPortFactory { get; }
+		public static IStaticData StaticData { get; }
 
 		// TODO: This extra namespace for OSInformation is unnecessary. The one property that is on this item should be propagated to the Configuration class
 		public static OsInformationProvider OsInformation { get; }
 
 		static Configuration()
 		{
-			string typeString;
+			PlatformConfig config;
 
-			// Initialize the OsInformation Provider
-			typeString = "MatterControl.Agg.PlatformAbstract.OsInformationWindowsPlugin, agg_platform_win32, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
-			OsInformation = LoadProviderFromAssembly<OsInformationProvider>(typeString);
-			if (OsInformation == null)
+			if(File.Exists("AggPlatform1.json"))
 			{
-				throw new Exception(string.Format("Unable to load the OSInfo provider"));
+				// Use the config file from the file system if it exists
+				config = Newtonsoft.Json.JsonConvert.DeserializeObject<PlatformConfig>(File.ReadAllText("AggPlatform.json"));
+			}
+			else
+			{
+				// Use the default config embedded in the assembly if the file system override does not exist
+				using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("MatterHackers.Agg.Extensibility.DefaultPlatformConfig.json")))
+				{
+					config = Newtonsoft.Json.JsonConvert.DeserializeObject<PlatformConfig>(reader.ReadToEnd());
+				}
 			}
 
-			// Initialize the ImageIO Provider
-			typeString = "MatterHackers.Agg.Image.ImageIOWindowsPlugin, agg_platform_win32";
-			ImageIO = LoadProviderFromAssembly<ImageIOProvider>(typeString);
+			// OsInformation Provider
+			OsInformation = LoadProviderFromAssembly<OsInformationProvider>(config.Providers.OsInformationProvider);
+			if (OsInformation == null)
+			{
+				throw new Exception(string.Format("Unable to load the OsInformation provider"));
+			}
+
+			// ImageIO Provider
+			ImageIO = LoadProviderFromAssembly<ImageIOProvider>(config.Providers.ImageIOProvider);
 			if (ImageIO == null)
 			{
 				throw new Exception(string.Format("Unable to load the ImageIO provider"));
 			}
 
-			// Initialize the FileDialog Provider
-			typeString = "MatterHackers.Agg.PlatformAbstract.FileDialogPlugin, agg_platform_win32, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
-			FileDialogs = LoadProviderFromAssembly<IFileDialogProvider>(typeString);
+			// FileDialog Provider
+			FileDialogs = LoadProviderFromAssembly<IFileDialogProvider>(config.Providers.DialogProvider);
 			if (FileDialogs == null)
 			{
 				throw new Exception(string.Format("Unable to load the File Dialog provider"));
 			}
+
+			// FileDialog Provider
+			StaticData = LoadProviderFromAssembly<IStaticData>(config.Providers.StaticDataProvider);
+			if (StaticData == null)
+			{
+				throw new Exception(string.Format("Unable to load the StaticData provider"));
+			}
+		}
+
+		private class PlatformConfig
+		{
+			public ProviderConfig Providers { get; set; }
+			public Dictionary<string, string> Settings { get; set; }
+		}
+
+		private class ProviderConfig
+		{
+			public string OsInformationProvider { get; set; }
+			public string DialogProvider { get; set; }
+			public string ImageIOProvider { get; set; }
+			public string StaticDataProvider { get; set; }
 		}
 	}
 }
